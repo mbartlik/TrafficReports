5247519e45b298dd8e6600b33478c84bda1053b9
mbartlik <maxbartlik@gmail.com>
2025-03-24 18:39:39 -0400

    lint fix

--- frontend/src/components/routeDurationGraph.js
+++ frontend/src/components/routeDurationGraph.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { format, subHours, subDays, startOfHour, addHours } from 'date-fns';
+import { subHours, subDays, startOfHour, addHours } from 'date-fns';
 
 const RouteDurationGraph = ({ routeData, convertDurationStringToTime, convertToUserTimezone }) => {
   const [filteredData, setFilteredData] = useState([]);
