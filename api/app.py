57850373922b24ad25f39dd84e5a0cb158a379b3
mbartlik <maxbartlik@gmail.com>
2025-03-31 18:35:04 -0400

    remove api debug mode

--- api/app.py
+++ api/app.py
@@ -167,5 +167,5 @@ def ping():
     return "Ping successful!", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
-    # app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    # app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
